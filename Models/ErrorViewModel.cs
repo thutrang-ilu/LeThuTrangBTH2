0ba5c789187c5f13e14761cc8444115c78281d31
LeThuTrang <thutrang0612.ilu@gmail.com>
2022-10-06 20:18:29 +0700

    LeThuTrang0510

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LeThuTrangBTH2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
